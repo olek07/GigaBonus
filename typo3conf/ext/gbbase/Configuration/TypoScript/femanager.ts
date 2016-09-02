0727ab62f9c381a0310a4c80ba54db5b115054d7
Alexander Averbuch <alav@gmx.net>
2016-09-02 19:24:35 +0200

    - modified templates - modified typoscript and controller

--- typo3conf/ext/gbbase/Configuration/TypoScript/femanager.ts
+++ typo3conf/ext/gbbase/Configuration/TypoScript/femanager.ts
@@ -1,4 +1,6 @@
 plugin.tx_femanager {
+    view.partialRootPaths.10 = EXT:gbfemanager/Resources/Private/Partials/
+
     settings {
         new {
             validation {
@@ -28,12 +30,12 @@ plugin.tx_femanager {
 
     _LOCAL_LANG {
         # Default Language
-        ru.tx_femanager_domain_model_user.dateFormat = m.d.Y
-        ru.tx_femanager_domain_model_user.dateOfBirth.placeholder = m.d.Y
+        ru.tx_femanager_domain_model_user.dateFormat = d.m.Y
+        ru.tx_femanager_domain_model_user.dateOfBirth.placeholder = d.m.Y
 
         # For DE
-        ua.tx_femanager_domain_model_user.dateFormat = m.d.Y
-        ua.tx_femanager_domain_model_user.dateOfBirth.placeholder = m.d.Y
+        ua.tx_femanager_domain_model_user.dateFormat = d.m.Y
+        ua.tx_femanager_domain_model_user.dateOfBirth.placeholder = d.m.Y
     }
 
 }
--- typo3conf/ext/gbfemanager/Classes/Controller/EditController.php
+++ typo3conf/ext/gbfemanager/Classes/Controller/EditController.php
@@ -3,6 +3,31 @@ namespace Gigabonus\Gbfemanager\Controller;
 
 class EditController extends \In2code\Femanager\Controller\EditController {
     
+    
+    /**
+    * initialize create action
+    *
+    * @return void
+    */
+    public function initializeUpdateAction() {
+        if ($this->arguments->hasArgument('user')) {
+                $this->arguments->getArgument('user')
+                        ->getPropertyMappingConfiguration()
+                        ->skipProperties('day', 'month', 'year');
+        }
+    }
+    
+        /**
+     * action edit
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+        parent::editAction();
+    }
+    
+    
     /**
      * action update
      *
@@ -13,6 +38,12 @@ class EditController extends \In2code\Femanager\Controller\EditController {
      * @return void
      */
     public function updateAction(\Gigabonus\Gbfemanager\Domain\Model\User $user) {
+        $formData = $this->request->getArgument('user');
+        // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($formData);exit;
+                
+        $date = new \DateTime();
+        $date->setDate((int)$formData['year'], (int)$formData['month'], (int)$formData['day']);
+        $user->setDateOfBirth($date);
         parent::updateAction($user);
     }
 
