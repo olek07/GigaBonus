d2a6d1650c9986d8a78918a2281759ae093d8691
Alexander Averbuch <alav@gmx.net>
2016-10-23 23:54:48 +0200

    - customized TCA-Configuration - don't show categories with 0 partners

--- typo3conf/ext/gbaccount/Configuration/TCA/tx_gbaccount_domain_model_transaction.php
+++ typo3conf/ext/gbaccount/Configuration/TCA/tx_gbaccount_domain_model_transaction.php
@@ -33,7 +33,7 @@ return array(
 		'1' => array('showitem' => ''),
 	),
 	'columns' => array(
-	
+/*	
 		'sys_language_uid' => array(
 			'exclude' => 1,
 			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
@@ -48,6 +48,7 @@ return array(
 				),
 			),
 		),
+*/
 		'l10n_parent' => array(
 			'displayCond' => 'FIELD:sys_language_uid:>:0',
 			'exclude' => 1,
@@ -133,7 +134,7 @@ return array(
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'foreign_table' => 'tx_gbpartner_domain_model_partner',
-                                'foreign_table_where' => ' AND tx_gbpartner_domain_model_partner.sys_language_uid IN (-1, ###REC_FIELD_sys_language_uid###) ',
+                                'foreign_table_where' => ' AND tx_gbpartner_domain_model_partner.sys_language_uid IN (-1, 0) ',
 				'minitems' => 0,
 				'maxitems' => 1,
 			),
