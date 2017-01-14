5e280cf7ee740758cf237d44bbf3edb0248cd273
Alexander Averbuch <alav@gmx.net>
2017-01-14 23:08:39 +0100

    powermail upgrade to 3.10.1

--- typo3conf/ext/powermail/ext_emconf.php
+++ typo3conf/ext/powermail/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "powermail".
  *
- * Auto generated 02-01-2017 12:28
+ * Auto generated 14-01-2017 23:05
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
         and easy to use mailform extension with a lots of features
         (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
   'category' => 'plugin',
-  'version' => '3.10.0',
+  'version' => '3.10.1',
   'state' => 'stable',
   'uploadfolder' => false,
   'createDirs' => 'uploads/tx_powermail,typo3temp/tx_powermail',
