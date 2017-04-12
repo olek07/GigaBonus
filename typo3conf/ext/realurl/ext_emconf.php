cc5fcb6ca23b211cd08f6cb85816adb377219bc7
Alexander Averbuch <alav@gmx.net>
2017-04-12 12:39:17 +0200

    upgrade realurl to 2.2.0

--- typo3conf/ext/realurl/ext_emconf.php
+++ typo3conf/ext/realurl/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "realurl".
  *
- * Auto generated 12-03-2017 11:49
+ * Auto generated 12-04-2017 12:37
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'Speaking URLs for TYPO3',
   'description' => 'Makes TYPO3 URLs search engine friendly. Donations are welcome to dmitry.dulepov@gmail.com. They help to support the extension!',
   'category' => 'services',
-  'version' => '2.1.9',
+  'version' => '2.2.0',
   'state' => 'stable',
   'uploadfolder' => true,
   'createDirs' => '',
@@ -26,9 +26,9 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-8.6.999',
-      'php' => '5.4.0-7.0.999',
-      'scheduler' => '6.2.0-8.6.999',
+      'typo3' => '6.2.0-8.7.999',
+      'php' => '5.4.0-7.1.999',
+      'scheduler' => '6.2.0-8.7.999',
     ),
     'conflicts' => 
     array (
