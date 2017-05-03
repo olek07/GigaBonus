8ca21398f3411916f0fd6f9529cb0fb937d51567
Alexander Averbuch <aaverbuch@passionfive.de>
2017-05-03 18:52:46 +0200

    - customized/added templates and layouts - customized typoscript

--- typo3conf/ext/gbbase/Configuration/TypoScript/templates.ts
+++ typo3conf/ext/gbbase/Configuration/TypoScript/templates.ts
@@ -7,6 +7,8 @@
 # plugin.fluid.view.widget.TYPO3\CMS\Fluid\ViewHelpers\Widget\PaginateViewHelper.templateRootPath = EXT:gbbase/Resources/Private/Templates/
 
 
+
+### custom layout for content elements
 lib.fluidContent.layoutRootPaths.30 = EXT:gbbase/Resources/Private/Layouts/fluid_styled_content
 
 page = PAGE
@@ -14,23 +16,26 @@ page.typeNum = 0
 
 page.meta.viewport = initial-scale=1, minimum-scale=1, maximum-scale=1
 
-### page.10 < lib.langMenu
 
-temp.languageSwitch < tt_content.list.20.gblanguages_languagemenu
-temp.languageSwitch = USER
-temp.languageSwitch {
-
-}
-
-page.30 = FLUIDTEMPLATE
-page.30 {
+page.10 = FLUIDTEMPLATE
+page.10 {
     format = html
     file = EXT:gbbase/Resources/Private/Templates/Page/Layouts/MainTemplate.html
     partialRootPath = EXT:gbbase/Resources/Private/Templates/Page/Partials/
-    # layoutRootPath = typo3conf/ext/gbbase/Resources/Private/Templates/Page/Layouts/
+    layoutRootPath = EXT:gbbase/Resources/Private/Templates/Page/Layouts/
+}
 
-    variables {
-        languageSwitch < temp.languageSwitch
-    }
+
+page.10.file.stdWrap.cObject = CASE
+page.10.file.stdWrap.cObject {
+    key.data = levelfield:-1, backend_layout_next_level, slide
+    key.override.field = backend_layout
+
+    default = TEXT
+    default.value = EXT:gbbase/Resources/Private/Templates/Page/Layouts/MainTemplate.html
+
+    2 = TEXT
+    2.value = EXT:gbbase/Resources/Private/Templates/Page/Layouts/TwoColumnWidthLeftNaviTemplate.html
+
 
 }
