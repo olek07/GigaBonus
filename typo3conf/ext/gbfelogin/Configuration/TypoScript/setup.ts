fbecaabf5b26a9330a7914aa2d7afa7ba0b4b3e9
Alexander Averbuch <aaverbuch@passionfive.de>
2017-05-22 22:14:32 +0200

    - customized css

--- typo3conf/ext/gbfelogin/Configuration/TypoScript/setup.ts
+++ typo3conf/ext/gbfelogin/Configuration/TypoScript/setup.ts
@@ -3,6 +3,13 @@ plugin.tx_felogin_pi1 {
   templateFile = EXT:gbfelogin/Resources/Private/Templates/FrontendLogin.html
   forgotResetMessageEmailSentMessage_stdWrap.htmlSpecialChars = 0
 
+  errorMessage_stdWrap {
+    required = 1
+    wrap = <div class="error-text">|</div>
+    htmlSpecialChars = 1
+  }
+
+
   linkConfig.useCacheHash = 1
 
   _CSS_DEFAULT_STYLE >
