24739c1dc29060b4e02dd35fbd87c2ccac68ed6d
Alexander Averbuch <aaverbuch@passionfive.de>
2017-05-28 15:54:48 +0200

    Integrated SASS/Compass

--- typo3conf/ext/gbbase/Configuration/TypoScript/includecss.ts
+++ typo3conf/ext/gbbase/Configuration/TypoScript/includecss.ts
@@ -1,5 +1,6 @@
 page.includeCSS {
-    foundation = EXT:gbbase/Resources/Public/Css/foundation.min.css
+    // foundation = EXT:gbbase/Resources/Public/Css/foundation.min.css
+    // foundation = https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css
     app = EXT:gbbase/Resources/Public/Css/app.css
     jqueryui = EXT:gbbase/Resources/Public/Css/Libs/jquery-ui/jquery-ui.css
 }
